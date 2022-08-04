3c42e28beab3d989063c81ba699871c9531e62b5
bader <bader2007as@hotmail.com>
2022-08-04 11:18:33 +0300

    lab1_day5_list

---
+++ lab1_day5_list.py
@@ -0,0 +1,12 @@
+lst=[5, 4, 17, 19, 30, 2, 7, 10, 45]
+
+
+
+
+print("Sum of all items= ",sum(lst))
+print("largest number from the list= ",max(lst))
+
+newlst=[print("even number = ",x)  for x in lst if x%2==0]
+print(lst[4:])
+
+
